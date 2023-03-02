8285561116776d3ef2050ed85e5d48073fac06c3
eslam adel <123577573+Eslamadel0@users.noreply.github.com>
2023-03-02 19:05:07 +0200

    Add files via upload

---
+++ Uint_2 C language/C basic/additional task/A3.c
@@ -0,0 +1,22 @@
+/*
+ * A3.c
+ *
+ *  Created on: Feb 26, 2023
+ *      Author: Eslam adel fathy
+Description : Write a program for converting temperature from degrees Celsius to degrees Fahrenheit,given the formula
+#include <stdio.h>
+#include <stdlib.h>
+int main (void){
+
+	setbuf (stdout,NULL);
+	// variable declaration
+	float temp,f;
+	printf(" enter temperature from degrees Celsius :\n");
+	scanf("%f",&temp);
+
+	//process
+	f=temp*(9.0/5.0)+32;
+	printf("the degrees Fahrenheit : %.2f \n",f);
+
+}
+*/
---
+++ Uint_2 C language/C basic/additional task/A5.c
@@ -0,0 +1,20 @@
+/*
+ * A5.c
+ *
+ *  Created on: Feb 26, 2023
+ *      Author: Eslam adel fathy
+Description :Write a program to print the ASCII value of a character 
+input by the user.
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+int main (void){
+
+	setbuf (stdout,NULL);
+	// variable declaration
+    char ch;
+    printf("enter  character you want to check ASCII value : \n");
+    scanf("%c",&ch);
+    printf("the ASCII value is : %d",ch);
+}
+ */
