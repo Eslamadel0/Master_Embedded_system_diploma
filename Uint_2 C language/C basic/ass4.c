c128ca1cf22a4903bd46bd70ea50746538832a3b
eslam adel <123577573+Eslamadel0@users.noreply.github.com>
2023-08-23 20:49:46 +0300

    Update ass4.c

--- Uint_2 C language/C basic/ass4.c
+++ Uint_2 C language/C basic/ass4.c
@@ -3,18 +3,18 @@
  *
  *  Created on: Feb 24, 2023
  *      Author: Eslam adel fathy
-
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int main (void){
 
+	//variable
 	int x;
 	printf("enter the number you want to check \n");
-    scanf("%d",&x);
-
+        scanf("%d",&x);
+        //process
     if(x>=0)
     	printf("the number is positive : %d \n",x);
     else
     	printf("the nmuber is negative : %d",x);
 }
-*/
