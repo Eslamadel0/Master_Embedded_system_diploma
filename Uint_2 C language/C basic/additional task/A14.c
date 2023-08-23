a99916f86d3c55ed340dc10bb88fd84a51ff382e
eslam adel <123577573+Eslamadel0@users.noreply.github.com>
2023-08-23 21:02:32 +0300

    Update A14.c

--- Uint_2 C language/C basic/additional task/A14.c
+++ Uint_2 C language/C basic/additional task/A14.c
@@ -4,6 +4,7 @@
  *  Created on: Feb 28, 2023
  *      Author: Eslam adel fathy
 Description :Write a program to display English alphabets from A to Z.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
@@ -18,4 +19,3 @@ int main(void){
 		printf(" %c\n",ch);
 	}
 	}
- */
