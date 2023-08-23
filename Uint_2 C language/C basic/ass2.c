7d406160ea81dec55dba4b295ea00b0c5d743428
eslam adel <123577573+Eslamadel0@users.noreply.github.com>
2023-08-23 20:48:16 +0300

    Update ass2.c

--- Uint_2 C language/C basic/ass2.c
+++ Uint_2 C language/C basic/ass2.c
@@ -3,20 +3,19 @@
  *
  *  Created on: Feb 24, 2023
  *      Author: Eslam adel fathy
-
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int main (void){
 
-
+	//variable
 	char c;
 	printf("enter the alphabet : \n");
 	scanf("%c",&c);
-
+        //process
 	if (c=='a'||c=='A'||c=='u'||c=='U'||c=='o'||c=='O'||c=='e'||c=='E'||c=='i'||c=='I')
 
 		printf("the alphabet is vowel \n");
 	else
 		printf("the alphabet is consonant \c");
 }
-*/
