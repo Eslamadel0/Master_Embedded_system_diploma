5861cd8da6ca4dc6bc3237f54731f17f75ae5a9f
eslam adel <123577573+Eslamadel0@users.noreply.github.com>
2023-08-23 20:52:12 +0300

    Update ass7.c

--- Uint_2 C language/C basic/ass7.c
+++ Uint_2 C language/C basic/ass7.c
@@ -3,16 +3,17 @@
  *
  *  Created on: Feb 24, 2023
  *      Author: Eslam adel fathy
-
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int main (void){
 
+	//variables
 	int x,y=1;
 	unsigned long fac=1;
 	printf("enter the number you want to find factorial \n");
 	scanf("%d",&x);
-
+        //process
 	if(x<0)
 		printf("the number is negative \n");
 	else
@@ -23,4 +24,3 @@ int main (void){
 	printf("factorial of number = %d  \n factorial = %u ",x,fac);
 
 }
- */
